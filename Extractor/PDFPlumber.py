a8d5ba3e189c603b5e4bcabb532f7c089b7614be
sarrabenyahia <93934407+sarrabenyahia@users.noreply.github.com>
2023-10-23 12:54:19 +0200

    clean venv

--- Extractor/PDFPlumber.py
+++ Extractor/PDFPlumber.py
@@ -6,22 +6,17 @@ def process_pdf(pdf_path):
     """
     Process a single PDF file and extract tables to save in an Excel file.
     """
-    # Read PDF and extract tables
     tables = camelot.read_pdf(pdf_path, pages='all', strip_text='\n')
 
-    # Print the number of tables extracted
     print(f"Processing {pdf_path}. Total tables extracted: {tables.n}")
 
-    # Set output Excel filename based on the PDF filename
     output_filename = os.path.splitext(os.path.basename(pdf_path))[0] + '.xlsx'
     output_filepath = os.path.join(os.path.dirname(pdf_path), output_filename)
 
     with pd.ExcelWriter(output_filepath) as writer:
         for i, table in enumerate(tables):
-            # Convert the table to a pandas DataFrame
             df = table.df
             
-            # Write the DataFrame to a new sheet in the Excel file
             df.to_excel(writer, sheet_name=f'Table_{i+1}', index=False)
 
     print(f"Saved tables from {pdf_path} to {output_filepath}")
@@ -30,13 +25,11 @@ def main(directory_path):
     """
     Process all PDF files in the specified directory.
     """
-    # Loop through all files in the directory
     for filename in os.listdir(directory_path):
         if filename.endswith('.pdf'):
             pdf_path = os.path.join(directory_path, filename)
             process_pdf(pdf_path)
 
 if __name__ == '__main__':
-    # Set your directory path here
     directory_path = './pdf'
     main(directory_path)
