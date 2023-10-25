cea492fcaf2c76876b6339e488eb30c640f4118c
sarrabenyahia <93934407+sarrabenyahia@users.noreply.github.com>
2023-10-25 17:34:59 +0200

    date & outliers detected

--- QuickScanEFR/TextMachina/DateFromatter.py
+++ QuickScanEFR/TextMachina/DateFromatter.py
@@ -2,8 +2,6 @@ import os
 import pandas as pd
 import re
 
-# LA ligne "DATE" peut ne pas être une date carrément, et juste c'est écrit "6 mois plus tard...", il faudrait recalculer ?
-
 class DateFormatter:
 
     @staticmethod
@@ -16,22 +14,49 @@ class DateFormatter:
         date_pattern = re.compile(r'(\d{1,2}[./]\d{1,2}[./]\d{2})')
         match = date_pattern.search(s)
         return match.group(1) if match else s
+    
+    @staticmethod
+    def extract_relative_date(reference_date, s):
+        """
+        Extracts a relative date based on a reference date and a string indicating the offset.
+        Handles variations like "1 an plus tard", "6 mois", "2 ans", "4 jours après", etc.
+        """
+        num = int(re.search(r'(\d+)', s).group(1))
+        if "jour" in s:
+            new_date = reference_date + pd.DateOffset(days=num)
+        elif "mois" in s:
+            new_date = reference_date + pd.DateOffset(months=num)
+        elif "an" in s or "ans" in s:
+            new_date = reference_date + pd.DateOffset(years=num)
+        else:
+            new_date = reference_date
+        return new_date
 
     @staticmethod
     def flexible_date_formatting(date_series):
         """
         Attempt to format a series of dates using a more simplified approach.
-        Rely on 'dayfirst' argument to handle various day/month formats.
+        Handles relative date terms and relies on 'dayfirst' argument to handle various day/month formats.
         """
-        date_series = date_series.map(lambda x: DateFormatter.extract_date_from_string(str(x)))
-        date_series = pd.to_datetime(date_series, errors='coerce', dayfirst=True)
-        return date_series.dt.strftime('%d/%m/%y')
+        cleaned_dates = []
+        for idx, date_str in enumerate(date_series):
+            date_str = str(date_str)
+            if any(term in date_str for term in ["plus tard", "après", "mois", "an", "ans", "jour"]):
+                new_date = DateFormatter.extract_relative_date(cleaned_dates[-1], date_str)
+                cleaned_dates.append(new_date)
+            else:
+                date = DateFormatter.extract_date_from_string(date_str)
+                try:
+                    cleaned_dates.append(pd.to_datetime(date, dayfirst=True))
+                except:
+                    cleaned_dates.append(date)
+        return pd.Series(cleaned_dates).dt.strftime('%d/%m/%y')
 
     @staticmethod
     def format_dates_in_worksheet(df):
         """Formats the dates in the first row of a DataFrame using a simplified approach."""
         formatted_dates = DateFormatter.flexible_date_formatting(df.iloc[0, 1:])
-        df.iloc[0, 1:] = formatted_dates
+        df.iloc[0, 1:len(formatted_dates)+1] = formatted_dates
         return df
 
     @classmethod
@@ -42,19 +67,37 @@ class DateFormatter:
         for filename in os.listdir(directory):
             if filename.endswith(".xlsx"):
                 file_path = os.path.join(directory, filename)
-                
                 with pd.ExcelFile(file_path) as xls:
                     all_sheets = {}
-                    
                     for sheet_name in xls.sheet_names:
                         df = pd.read_excel(xls, sheet_name)
                         cleaned_df = cls.format_dates_in_worksheet(df)
                         all_sheets[sheet_name] = cleaned_df
-                        
                 output_file_path = os.path.join(directory, filename.replace(".xlsx", "_cleaned.xlsx"))
                 with pd.ExcelWriter(output_file_path) as writer:
                     for sheet_name, data in all_sheets.items():
                         data.to_excel(writer, sheet_name=sheet_name, index=False)
-                
                 print(f"Processed: {filename} -> {filename.replace('.xlsx', '_cleaned.xlsx')}")
+    @staticmethod
+    def extract_relative_date(reference_date, s):
+        """
+        Extracts a relative date based on a reference date and a string indicating the offset.
+        Handles variations like "1 an plus tard", "6 mois", "2 ans", "4 jours après", etc.
+        """
+        match = re.search(r'(\d+)', s)
+        if match:
+            num = int(match.group(1))
+        else:
+            # Setting num to a default value (can be changed as per requirements)
+            num = 0
+
+        if "jour" in s:
+            new_date = reference_date + pd.DateOffset(days=num)
+        elif "mois" in s:
+            new_date = reference_date + pd.DateOffset(months=num)
+        elif "an" in s or "ans" in s:
+            new_date = reference_date + pd.DateOffset(years=num)
+        else:
+            new_date = reference_date
+        return new_date
 
--- QuickScanEFR/TextMachina/main.py
+++ QuickScanEFR/TextMachina/main.py
@@ -1,18 +1,24 @@
 from PercentageExtractor import PercentageExtractor
 from DateFromatter import DateFormatter
 from PDFPlumber import PDFProcessor 
+from OutlierDetector import OutlierDetector
 
 # Initialization
 date_formatter = DateFormatter()
 percent_extractor = PercentageExtractor()
-pdf_processor = PDFProcessor(directory_path='../QuickScanEFR/pdf_TextMachina')
+pdf_processor = PDFProcessor(directory_path='../pdf_TextMachina/')
 
-# First, process the PDFs to extract tables and save them in Excel format
+# Process the PDFs to extract tables and save them in Excel format
 pdf_processor.process_directory()
 
-# Then, format the dates
-date_formatter.format_dates_in_excel("../QuickScanEFR/pdf_TextMachina")
+# Format the dates
+excel_file_path = "../pdf_TextMachina/"  # Replace with the actual Excel file name if needed
+date_formatter.format_dates_in_excel(excel_file_path)
 
-# Finally, extract the percentages
-percent_extractor.extract_percentages_from_excel("../QuickScanEFR/pdf_TextMachina")
+# Extract the percentages
+percent_extractor.extract_percentages_from_excel(excel_file_path)
 
+# Initialize and use the OutlierDetector for multiple documents
+outlier_detector = OutlierDetector(None)
+directory_path = "../pdf_TextMachina/"
+outlier_detector.detect_anomalies_for_multiple_docs(directory_path)
