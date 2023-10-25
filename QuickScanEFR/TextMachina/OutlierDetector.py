c2c2b96954e8c0dd1bed3d80f472acfcc11000ef
sarrabenyahia <93934407+sarrabenyahia@users.noreply.github.com>
2023-10-25 18:15:29 +0200

    outlierdetector works :)

--- QuickScanEFR/TextMachina/OutlierDetector.py
+++ QuickScanEFR/TextMachina/OutlierDetector.py
@@ -8,19 +8,20 @@ class OutlierDetector:
     def __init__(self, excel_file_path=None, relative_size_threshold=5):
         self.excel_file_path = excel_file_path
         self.relative_size_threshold = relative_size_threshold
-        if excel_file_path:
-            self.sheet_names = pd.ExcelFile(excel_file_path).sheet_names
-            self.combined_data = self._combine_data_horizontally()
         self.anomalies_dict = {}
+        if excel_file_path:
+            self.workbook = load_workbook(excel_file_path)
+            self.sheet_names = self.workbook.sheetnames
+            self.combined_data = self._combine_data_horizontally()
     
     def _combine_data_horizontally(self):
-        combined_data = pd.read_excel(self.excel_file_path, sheet_name=self.sheet_names[0])
+        combined_data = pd.DataFrame([cell for cell in col] for col in self.workbook[self.sheet_names[0]].iter_cols(values_only=True)).T
         for sheet in self.sheet_names[1:]:
-            temp_df = pd.read_excel(self.excel_file_path, sheet_name=sheet)
+            temp_df = pd.DataFrame([cell for cell in col] for col in self.workbook[sheet].iter_cols(values_only=True)).T
             temp_df = temp_df.drop(temp_df.columns[0], axis=1)
             combined_data = pd.concat([combined_data, temp_df], axis=1)
         return combined_data
-    
+
     @staticmethod
     def convert_to_float(val):
         try:
@@ -32,20 +33,6 @@ class OutlierDetector:
             return np.nan
     
     def detect_relative_median_anomalies(self):
-        """
-        Detects anomalies in the data based on the relative size compared to a rolling median.
-        
-        The method iterates through each row (metric) in the combined data. For each metric, 
-        it calculates a rolling median using a window of 3 (the value itself, the previous value, 
-        and the next value). 
-        The relative size of each value to its corresponding rolling median 
-        is then computed. 
-        If the relative size exceeds the predefined threshold or is below the 
-        inverse of the threshold, the value is flagged as an anomaly.
-        
-        Detected anomalies are stored in the `anomalies_dict` attribute of the class with the metric 
-        name as the key and the anomalous values as the associated values.
-        """
         for index, row in self.combined_data.iterrows():
             metric_name = row[0]
             if index == 0 or pd.isnull(metric_name):
@@ -59,27 +46,28 @@ class OutlierDetector:
                 self.anomalies_dict[metric_name] = anomalies
                 
     def highlight_anomalies_in_excel(self, output_file_path=None):
-        workbook = load_workbook(self.excel_file_path)
         red_fill = PatternFill(start_color="FF0000", end_color="FF0000", fill_type="solid")
         
         for sheet_name in self.sheet_names:
-            worksheet = workbook[sheet_name]
+            worksheet = self.workbook[sheet_name]
             for row in worksheet.iter_rows(min_row=2, min_col=2, max_col=worksheet.max_column):
                 for cell in row:
                     metric = worksheet.cell(row=cell.row, column=1).value
-                    if metric in self.anomalies_dict and cell.value in self.anomalies_dict[metric].values:
+                    cell_value = self.convert_to_float(cell.value)
+                    if metric in self.anomalies_dict and cell_value in self.anomalies_dict[metric].values:
                         cell.fill = red_fill
         
         if output_file_path is None:
             output_file_path = self.excel_file_path.replace(".xlsx", "_modified.xlsx")
-        workbook.save(output_file_path)
+        self.workbook.save(output_file_path)
 
     def detect_anomalies_for_multiple_docs(self, directory_path):
         excel_files = [f for f in os.listdir(directory_path) if f.endswith('_cleaned.xlsx')]
         
         for excel_file in excel_files:
             self.excel_file_path = os.path.join(directory_path, excel_file)
-            self.sheet_names = pd.ExcelFile(self.excel_file_path).sheet_names
+            self.workbook = load_workbook(self.excel_file_path)
+            self.sheet_names = self.workbook.sheetnames
             self.combined_data = self._combine_data_horizontally()
             self.detect_relative_median_anomalies()
             self.highlight_anomalies_in_excel()
