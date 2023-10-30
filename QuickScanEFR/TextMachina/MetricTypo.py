d35c860008a18697731645cbb6d88015fa7f2fb6
sarrabenyahia <93934407+sarrabenyahia@users.noreply.github.com>
2023-10-30 11:39:18 +0100

    levenshtein distance

---
+++ QuickScanEFR/TextMachina/MetricTypo.py
@@ -0,0 +1,46 @@
+
+class TypoCorrector:
+    def __init__(self, standardized_strings):
+        """
+        Initialize the TypoCorrector with a list of standardized strings.
+        
+        :param standardized_strings: List of standardized strings to be used for correction.
+        """
+        self.standardized_strings = standardized_strings
+
+    @staticmethod
+    def levenshtein_distance(s1, s2):
+        """Compute the Levenshtein distance between two strings."""
+        if len(s1) > len(s2):
+            s1, s2 = s2, s1
+
+        distances = range(len(s1) + 1)
+        for index2, char2 in enumerate(s2):
+            new_distances = [index2 + 1]
+            for index1, char1 in enumerate(s1):
+                if char1 == char2:
+                    new_distances.append(distances[index1])
+                else:
+                    new_distances.append(1 + min((distances[index1], distances[index1 + 1], new_distances[-1])))
+            distances = new_distances
+
+        return distances[-1]
+    
+    def correct(self, string, relevant_metrics, max_distance=3):
+        """
+        Correct the given string based on Levenshtein distance and a threshold.
+        
+        :param string: The string to be corrected.
+        :param threshold_ratio: Threshold for correction based on the ratio of string length.
+        :return: Corrected string.
+        """
+        best_match = None
+        best_distance = float('inf')
+        for standard in relevant_metrics:
+            distance = self.levenshtein_distance(string, standard)
+            if distance < best_distance:
+                best_distance = distance
+                best_match = standard
+        if best_distance <= max_distance:
+            return best_match
+        return string
--- QuickScanEFR/TextMachina/main.py
+++ QuickScanEFR/TextMachina/main.py
@@ -1,4 +1,5 @@
 import os
+import pandas as pd
 from PercentageExtractor import PercentageExtractor
 from DateFormatter import DateFormatter
 from PDFPlumber import PDFProcessor
@@ -6,6 +7,13 @@ from OutlierDetector import OutlierDetector
 from VEMSImputer import VEMSCorrector
 from UnitCorrector import UnitCorrector
 from ExcelCleanup import ExcelCleanup
+from MetricTypo import TypoCorrector
+
+standardized_metrics = [
+    "CV", "CVF", "VR Helium", "VR  Plethysmo", "CPT Helium", "CPT Plethysmo",
+    "VR/CPT", "VEMS", "VEMS/CV", "DEM 25-75", "AA/O²", "PaO²", "PaCO²", "pH",
+    "TLCO", "TLCO/Va", "AA/O²", "Walk Test", "SaO² mini", "PI", "Arrêt inter"
+]
 
 class MainPipeline:
     def __init__(self, directory_path):
@@ -17,6 +25,7 @@ class MainPipeline:
         self.vems_corrector = VEMSCorrector()
         self.unit_corrector = UnitCorrector()
         self.excel_cleanup = ExcelCleanup()
+        self.typo_corrector = TypoCorrector(standardized_metrics)
 
 
     def process_pdfs(self):
@@ -46,6 +55,28 @@ class MainPipeline:
     def clean_excel_files(self):
         self.excel_cleanup.clean_multiple_docs(self.directory_path)
 
+    def correct_typos_in_excel(self):
+        """Correct typos in the metric names in Excel files."""
+        for filename in os.listdir(self.directory_path):
+            if filename.endswith("_cleaned.xlsx"):
+                filepath = os.path.join(self.directory_path, filename)
+                df = pd.read_excel(filepath)
+                
+                # Extract unique metric names from the document
+                unique_metrics = df[0].dropna().unique().tolist()
+                
+                # Determine the closest standardized names for each unique metric
+                relevant_metrics = [self.typo_corrector.correct(metric, self.typo_corrector.standardized_strings) 
+                                    for metric in unique_metrics]
+                
+                # Correct potential typos using the relevant metrics
+                df[0] = df[0].apply(lambda x: self.typo_corrector.correct(str(x), relevant_metrics) if pd.notnull(x) else x)
+                
+                # Save the corrected dataframe back to the file using ExcelWriter
+                with pd.ExcelWriter(filepath) as writer:
+                    df.to_excel(writer, index=False)
+
+                
     def cleanup(self):
         """Remove intermediate files after the entire pipeline execution."""
         intermediate_suffixes = ["_cleaned.xlsx", "_cleaned_modified.xlsx", "_corrected.xlsx", "_cvf.xlsx"]
@@ -54,11 +85,10 @@ class MainPipeline:
                 if filename.endswith(suffix):
                     os.remove(os.path.join(self.directory_path, filename))
 
-
-
     def run(self):
         self.process_pdfs()
         self.format_dates()
+        self.correct_typos_in_excel()
         self.extract_percentages()
         self.detect_anomalies()
         self.correct_vems()
