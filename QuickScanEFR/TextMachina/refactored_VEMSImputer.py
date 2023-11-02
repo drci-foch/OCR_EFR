af76be76cd36c922a7d5118a4ed2dae4ce8b5c25
sarrabenyahia <93934407+sarrabenyahia@users.noreply.github.com>
2023-11-02 12:06:38 +0100

    suppress combined

--- QuickScanEFR/TextMachina/refactored_VEMSImputer.py
+++ QuickScanEFR/TextMachina/refactored_VEMSImputer.py
@@ -5,24 +5,24 @@ import pandas as pd
 class VEMSCorrector:
     def __init__(self, file_path=None):
         self.file_path = file_path
-        self.combined_data = None
+        self.data = None
 
     def load_workbook(self):
-        self.combined_data = pd.read_excel(self.file_path)
+        self.data = pd.read_excel(self.file_path)
 
     @staticmethod
     def is_percent_value_usable(value):
         return isinstance(value, (int, float)) and 0 <= value <= 100
 
     def compute_missing_vems(self):
-        vems_row = self.combined_data[self.combined_data[0] == 'VEMS'].iloc[0]
+        vems_row = self.data[self.data[0] == 'VEMS'].iloc[0]
         for idx, val in enumerate(vems_row[1:]):
             if not self.is_percent_value_usable(val):
-                self.combined_data.iat[vems_row.name, idx + 1] = None
+                self.data.iat[vems_row.name, idx + 1] = None
 
-    def save_combined_data(self, save_path=None):
-        save_path = save_path or self.file_path.replace(".xlsx", "_vems.xlsx")
-        self.combined_data.to_excel(save_path, index=False)
+    def save_data(self, save_path=None):
+        save_path = save_path or self.file_path.replace(".xlsx", ".xlsx")
+        self.data.to_excel(save_path, index=False)
 
     @staticmethod
     def correct_multiple_docs(directory_path):
@@ -32,4 +32,4 @@ class VEMSCorrector:
                 corrector = VEMSCorrector(file_path)
                 corrector.load_workbook()
                 corrector.compute_missing_vems()
-                corrector.save_combined_data()
+                corrector.save_data()
