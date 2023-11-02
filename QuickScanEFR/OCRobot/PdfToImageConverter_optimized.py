d6fb7b1bd3dbc12aa5b16b5291c365c36d5d5843
sarrabenyahia <93934407+sarrabenyahia@users.noreply.github.com>
2023-11-02 17:03:46 +0100

    Update PdfToImageConverter_optimized.py

--- QuickScanEFR/OCRobot/PdfToImageConverter_optimized.py
+++ QuickScanEFR/OCRobot/PdfToImageConverter_optimized.py
@@ -6,7 +6,7 @@ from concurrent.futures import ProcessPoolExecutor
 class PdfConverter:
     def __init__(self, folder_path):
         self.folder_path = folder_path
-        self.output_folder = os.path.join(folder_path, "converted_images")
+        self.output_folder = os.path.join(folder_path)
 
     def convert_pdf_to_image(self, pdf_path, output_folder):
         base_name = os.path.splitext(os.path.basename(pdf_path))[0] + '_'
@@ -14,7 +14,6 @@ class PdfConverter:
         for i, image in enumerate(images):
             image_path = os.path.join(output_folder, f'{base_name}{i}.png')
             image.save(image_path, 'PNG')
-        return f"Processed {os.path.basename(pdf_path)}"
 
     def convert_pdfs_to_images(self):
         if not os.path.exists(self.output_folder):
@@ -28,6 +27,4 @@ class PdfConverter:
                                          for pdf_file in pdf_files],
                                         [self.output_folder]*len(pdf_files)))
 
-            for message in results:
-                print(message)
 
