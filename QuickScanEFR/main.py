1b54c34d2831299cb6a967021cc1cd0926779eef
sarrabenyahia <93934407+sarrabenyahia@users.noreply.github.com>
2023-11-02 17:04:02 +0100

    Update main.py

--- QuickScanEFR/main.py
+++ QuickScanEFR/main.py
@@ -11,33 +11,33 @@ pdf_dir = os.path.join(base_dir, 'pdf')
 pdf_textmachina_dir = os.path.join(base_dir, 'pdf_TextMachina')
 pdf_ocrobot_dir = os.path.join(base_dir, 'pdf_OCRobot')
 
-# Create the folders if they don't exist
-if not os.path.exists(pdf_textmachina_dir):
-    os.makedirs(pdf_textmachina_dir)
+# # Create the folders if they don't exist
+# if not os.path.exists(pdf_textmachina_dir):
+#     os.makedirs(pdf_textmachina_dir)
 
-if not os.path.exists(pdf_ocrobot_dir):
-    os.makedirs(pdf_ocrobot_dir)
+# if not os.path.exists(pdf_ocrobot_dir):
+#     os.makedirs(pdf_ocrobot_dir)
 
-# Check each PDF in the pdf directory
-for filename in os.listdir(pdf_dir):
-    if filename.endswith('.pdf'):
-        filepath = os.path.join(pdf_dir, filename)
+# # Check each PDF in the pdf directory
+# for filename in os.listdir(pdf_dir):
+#     if filename.endswith('.pdf'):
+#         filepath = os.path.join(pdf_dir, filename)
         
-        with open(filepath, 'rb') as file:
-            reader = PyPDF2.PdfReader(file)
+#         with open(filepath, 'rb') as file:
+#             reader = PyPDF2.PdfReader(file)
             
-            # Extract text from all pages of the PDF
-            text = ''
-            for page in reader.pages:
-                text += page.extract_text()
+#             # Extract text from all pages of the PDF
+#             text = ''
+#             for page in reader.pages:
+#                 text += page.extract_text()
             
-            # Check if text is extracted
-            if text.strip():
-                # Move to pdf_TextMachina
-                os.rename(filepath, os.path.join(pdf_textmachina_dir, filename))
-            else:
-                # Move to pdf_OCRobot
-                os.rename(filepath, os.path.join(pdf_ocrobot_dir, filename))
+#             # Check if text is extracted
+#             if text.strip():
+#                 # Move to pdf_TextMachina
+#                 os.rename(filepath, os.path.join(pdf_textmachina_dir, filename))
+#             else:
+#                 # Move to pdf_OCRobot
+#                 os.rename(filepath, os.path.join(pdf_ocrobot_dir, filename))
 
 
 # Run OCRobot/main.py
