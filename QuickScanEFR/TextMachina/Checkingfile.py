b5cd7c14b6e858ca477688bf4547079b36bd0dfa
sarrabenyahia <93934407+sarrabenyahia@users.noreply.github.com>
2023-11-02 11:19:31 +0100

    keeping last date

--- QuickScanEFR/TextMachina/Checkingfile.py
+++ QuickScanEFR/TextMachina/Checkingfile.py
@@ -2,8 +2,8 @@ import PyPDF2
 import camelot
 import shutil
 import os
+from collections import defaultdict
 
-## TODO : Keep only last one in date for each IPP
 
 def is_selectable_text(pdf_path):
     # Check if the PDF contains selectable text using PyPDF2
@@ -16,43 +16,62 @@ def is_selectable_text(pdf_path):
                 return True
     return False
 
+
 def does_not_contain_service_efr(pdf_path):
-    # Check if the PDF does not contain "SERVICE EFR" in any of its pages
+    # Check if the PDF does not contain "SERVICE EFR" or "HOPITAL FOCH" in any of its pages
     with open(pdf_path, 'rb') as pdf_file:
         pdf_reader = PyPDF2.PdfReader(pdf_file)
         for page_num in range(len(pdf_reader.pages)):
             page = pdf_reader.pages[page_num]
             text = page.extract_text()
-            if "SERVICE EFR" in text.upper():  # Case-insensitive check
+            if "SERVICE EFR" in text.upper() or "HOPITAL FOCH" in text.upper():  # Case-insensitive check
                 return False
     return True
 
+
 def extract_tables(pdf_path):
     if is_selectable_text(pdf_path) and does_not_contain_service_efr(pdf_path):
-        # Use Camelot to extract tables from PDF with selectable text and no "SERVICE EFR"
+        # Use Camelot to extract tables from PDF with selectable text and no "SERVICE EFR" or "HOPITAL FOCH"
         tables = camelot.read_pdf(pdf_path, flavor='stream', pages='all')
         return tables
 
+
+def get_id_and_date_from_filename(filename):
+    # Assuming filenames are in the format "IPP_date_other-id-for-specific-document.pdf"
+    parts = filename.split('_')
+    if len(parts) >= 2:
+        return parts[0], parts[1]
+    return None, None
+
+
 def copy_pdfs_with_criteria(input_folder, output_folder):
+    # Default date to older one
+    latest_files = defaultdict(lambda: ('', '0000-00-00'))
+
     for root, _, files in os.walk(input_folder):
         for filename in files:
             if filename.endswith('.pdf'):
                 pdf_path = os.path.join(root, filename)
                 if is_selectable_text(pdf_path) and does_not_contain_service_efr(pdf_path):
-                    shutil.copy(pdf_path, os.path.join(output_folder, filename))
+                    id, date = get_id_and_date_from_filename(filename)
+                    # If this file is newer than the one stored
+                    if date > latest_files[id][1]:
+                        latest_files[id] = (filename, date)
+
+    for id, (filename, date) in latest_files.items():
+        shutil.copy(os.path.join(input_folder, filename),
+                    os.path.join(output_folder, filename))
+
 
 def main():
-    input_folder = "../pdf_TextMachina"  # Replace with the source folder path
-    output_folder = "../pdf_selection"  # Replace with the destination folder path
+    input_folder = "../pdf_TextMachina"
+    output_folder = "../pdf_selection"
 
-    # Create the output folder if it doesn't exist
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
 
     copy_pdfs_with_criteria(input_folder, output_folder)
 
+
 if __name__ == "__main__":
     main()
-
-
-
