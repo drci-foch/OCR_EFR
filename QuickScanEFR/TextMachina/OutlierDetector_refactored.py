b0c4c0e9a1d65aac94c8b5ce997fb209308867d5
sarrabenyahia <93934407+sarrabenyahia@users.noreply.github.com>
2023-11-02 12:06:09 +0100

    suppress combined

--- QuickScanEFR/TextMachina/OutlierDetector_refactored.py
+++ QuickScanEFR/TextMachina/OutlierDetector_refactored.py
@@ -1,4 +1,3 @@
-
 import os
 import pandas as pd
 from openpyxl import load_workbook
@@ -8,28 +7,27 @@ class OutlierDetector:
     def __init__(self, excel_file_path=None, threshold=0.5):
         self.excel_file_path = excel_file_path
         self.threshold = threshold
-        self.combined_data = None
+        self.data = None
         if excel_file_path:
             self._load_workbook()
 
     def _load_workbook(self):
-        self.combined_data = pd.read_excel(self.excel_file_path)
+        self.data = pd.read_excel(self.excel_file_path)
 
     def detect_relative_median_anomalies(self):
         anomalies = []
         
-        # Convert non-numeric values to NaN for median calculation, but keep the original data intact
-        numeric_data = self.combined_data.apply(pd.to_numeric, errors='coerce')
+        # Convert non-numeric values to NaN for median calculation
+        numeric_data = self.data.apply(pd.to_numeric, errors='coerce')
         
         medians = numeric_data.median(axis=1, skipna=True)
-        for index, row in self.combined_data.iterrows():
+        for index, row in self.data.iterrows():
             for col, value in enumerate(row):
                 # Check if the value is numeric and the median is not NaN
                 if pd.notnull(value) and pd.notnull(medians[index]) and isinstance(value, (int, float)):
                     if abs(value - medians[index]) > self.threshold * medians[index]:
                         anomalies.append((index, col))
         return anomalies
-
 
     def highlight_anomalies_in_excel(self, anomalies):
         workbook = load_workbook(self.excel_file_path)
