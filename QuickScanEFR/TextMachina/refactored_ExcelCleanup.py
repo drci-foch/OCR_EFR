ca3b5bb08b5c2a6a98bd5145f623ab5a97233ba2
sarrabenyahia <93934407+sarrabenyahia@users.noreply.github.com>
2023-11-02 12:06:21 +0100

    suppress combined

--- QuickScanEFR/TextMachina/refactored_ExcelCleanup.py
+++ QuickScanEFR/TextMachina/refactored_ExcelCleanup.py
@@ -6,10 +6,21 @@ import re
 class ExcelCleanup:
     def __init__(self, file_path=None):
         self.file_path = file_path
-        self.combined_data = None
+        self.data = None
 
     def load_workbook(self):
-        self.combined_data = pd.read_excel(self.file_path)
+        self.data = pd.read_excel(self.file_path)
+
+    def transpose_data(self):
+        # Reset the index to make sure the dates become a regular column
+
+        df_transposed = self.data.transpose()
+        df_transposed.columns = df_transposed.iloc[0]
+        df_transposed = df_transposed.drop(df_transposed.index[0])
+        
+        self.data = df_transposed
+        return self.data  # Return for inspection
+
 
     @staticmethod
     def format_percentage(value, is_first_column=False):
@@ -22,21 +33,22 @@ class ExcelCleanup:
         return value
 
     def clean_percentage_values(self):
-        rows_to_process = self.combined_data[self.combined_data.iloc[:, 0].str.endswith('%', na=False)]
+        rows_to_process = self.data[self.data.iloc[:, 0].str.endswith('%', na=False)]
         for _, row in rows_to_process.iterrows():
-            for idx, col in enumerate(self.combined_data.columns):
-                self.combined_data.at[row.name, col] = ExcelCleanup.format_percentage(self.combined_data.at[row.name, col], is_first_column=(idx == 0))
+            for idx, col in enumerate(self.data.columns):
+                self.data.at[row.name, col] = ExcelCleanup.format_percentage(self.data.at[row.name, col], is_first_column=(idx == 0))
 
     def save_cleaned_data(self, save_path=None):
-        save_path = save_path or self.file_path.replace(".xlsx", "_final.xlsx")
-        self.combined_data.to_excel(save_path, index=False)
+        save_path = save_path or self.file_path.replace(".xlsx", ".xlsx")
+        self.data.to_excel(save_path, index=True)
 
     @staticmethod
     def clean_multiple_docs(directory_path):
         for filename in os.listdir(directory_path):
-            if filename.endswith("_cvf.xlsx"):
+            if filename.endswith(".xlsx"):
                 file_path = os.path.join(directory_path, filename)
                 cleaner = ExcelCleanup(file_path)
                 cleaner.load_workbook()
                 cleaner.clean_percentage_values()
+                cleaner.transpose_data()  
                 cleaner.save_cleaned_data()
